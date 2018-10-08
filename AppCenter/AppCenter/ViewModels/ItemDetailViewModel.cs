b8c97831116858914f91e13702352e889f2c5d5e
Sanjul <sanjull@xpanxion.co.in>
2018-10-08 15:37:30 +0530

    Add project files.

---
+++ AppCenter/AppCenter/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using AppCenter.Models;
+
+namespace AppCenter.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
